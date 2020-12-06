f3c41066d699ffe398d0ad72e98abebb804cc910
shibinbin <15596186979@163.com>
2020-12-06 15:06:42 +0800

    'dsd'

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class DemoApplication implements ApplicationRunner {
+
+
+    private static ApplicationContext applicationContext;
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+		Arrays.stream(beanDefinitionNames).forEach(System.out::println);
+	}
+}
